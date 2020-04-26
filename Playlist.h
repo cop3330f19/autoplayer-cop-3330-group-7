a916b8daf673181e44814be4b4d13b56f6bf1b9c
andiadark <54601366+andiadark@users.noreply.github.com>
2020-04-26 11:36:07 -0400

    Update Playlist.h

--- Playlist.h
+++ Playlist.h
@@ -1,27 +1,47 @@
+/*****************************************************
+ *  File Name: Playlist.cpp
+ *  Group Number:7
+ *  Group Member Names:Andia Dark
+ *  Date last edited: 
+ *  Purpose: 
+ ****************************************************/
+
 #include <iostream>
 #include <string>
 #include "Song.h"
-
+#include <vector>
 class Playlist
 {
 
 public:
 Playlist();
 Playlist(std::string playlistName);
-void setPlaylistName(string playlistName)
-string getPlaylistName();
+void setPlaylistName(std::string playlistName);
+std::string getPlaylistName();
 void addSong(Song &);
 void deleteSong(Song &);
-std::string intersect(Playlist & );
-std::string Merge(Playlist &);
-void Play(Playlist &);
+Playlist intersect(Playlist & );
+Playlist Merge(std:: vector<Playlist> &);
+void play();
+void setMode(char mode);
+friend std::ostream& operator<<(std::ostream& os, const Playlist& list);
+friend std::ofstream& operator<<(std::ofstream& os, const Playlist& list);
+friend std::istream& operator>>(std::istream& is, Playlist& list);
+friend std::fstream& operator>>(std::fstream& is, Playlist& list);
+friend Playlist operator+ (Playlist&, Song&);
+friend Playlist operator+ (Playlist&, Playlist&);
+friend Playlist operator- (Playlist&, Song&);
+
+
 
 private:
 std::string playlistName;
-std::vector<Song> playlist;
+std::vector<Song> playlist;//holds list of song that belongs to playlist
+static char mode;
+int location;
 
 
-}
+};
 
 
 
